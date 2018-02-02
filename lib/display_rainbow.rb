67ee08559f641c9f901653dd3598180a3cf994e8
Cedric Niguidula <cedcn-9mail@yahoo.com>
2018-02-02 07:58:45 +0000

    automatically backed up by learn

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,5 +1,5 @@
 def display_rainbow(col)
-  puts "R: #{col[0]}, O: #{col[1]}, Y: #{col[2]}, G: #{col[3]},  B: #{col[4]}, I: #{col[5]}, V: #{col[6]}"
+  puts "R: #{col[0]}, O: #{col[1]}, Y: #{col[2]}, G: #{col[3]}, B: #{col[4]}, I: #{col[5]}, V: #{col[6]}"
 end
 colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet']
 display_rainbow(colors)
